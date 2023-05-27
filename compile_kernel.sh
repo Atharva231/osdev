44568101e7293b5cebb01230af70d207b2b01c0c
Atharva231 <dhandeatharva@gmail.com>
2023-05-27 12:51:59 +0530

    boot modification

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -4,7 +4,7 @@ nasm program_entry.asm -f elf32 -o program_entry.o
 nasm assembly_instructs.asm -f elf32 -o assembly_instructs.o
 nasm fat.asm -o fat.bin
 cd boot
-nasm boot_ap2.asm -o boot_ap2.bin
+nasm multi_core_boot.asm -o multi_core_boot.bin
 nasm ap_code.asm -o ap_code.bin
 nasm jumper_aid.asm -f elf32 -o jumper_aid.o
 gcc -fno-PIC -fno-stack-protector -m32 -s -c -o jumper.o jumper.c
